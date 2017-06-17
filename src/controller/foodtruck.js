81aa4d3786e66590a6ee31c1e836577b9d300e00
David Ferguson <git@dferguson.com>
2017-06-16 17:57:40 -0700

    Added iOS app lesson API changes

--- src/controller/foodtruck.js
+++ src/controller/foodtruck.js
@@ -16,7 +16,8 @@ export default({ config, db }) => {
         newFoodTruck.name = req.body.name;
         newFoodTruck.foodtype = req.body.foodtype;
         newFoodTruck.avgcost = req.body.avgcost;
-        newFoodTruck.geometry.coordinates = req.body.geometry.coordinates;
+        foodtruck.geometry.coordinates.lat = req.body.geometry.coordinates.lat;
+        foodtruck.geometry.coordinates.long = req.body.geometry.coordinates.long;
 
         newFoodTruck.save(err => {
             if (err) {
@@ -57,7 +58,8 @@ export default({ config, db }) => {
             foodtruck.name = req.body.name;
             foodtruck.foodtype = req.body.foodtype;
             foodtruck.avgcost = req.body.avgcost;
-            foodtruck.geometry.coordinates = req.body.geometry.coordinates;
+            foodtruck.geometry.coordinates.lat = req.body.geometry.coordinates.lat;
+            foodtruck.geometry.coordinates.long = req.body.geometry.coordinates.long;
 
             foodtruck.save(err => {
                 if (err) {
@@ -70,22 +72,34 @@ export default({ config, db }) => {
 
     // '/v1/foodtruck/:id' - Delete
     api.delete('/:id', authenticate, (req, res) => {
-        FoodTruck.remove({
-            _id: req.params.id
-        }, (err, foodtruck) => {
+        FoodTruck.findById(req.params.id, (err, foodtruck) => {
             if (err) {
-                res.send(err);
+                res.status(500).send(err);
+                return;
             }
-            Review.remove({
-                foodtruck: req.params.id
-            }, (err, review) => {
+            if (foodtruck === null) {
+                res.status(404).send("Food Truck not found");
+                return;
+            }
+            FoodTruck.remove({
+                _id: req.params.id
+            }, (err, foodtruck) => {
                 if (err) {
-                    res.send(err);
+                    res.status(500).send(err);
+                    return;
                 }
-                res.json({ message: "Food Truck successfully removed!"});
+                Review.remove({
+                    foodtruck: req.params.id
+                }, (err, review) => {
+                    if (err) {
+                        res.send(err);
+                    }
+                    res.json({ message: "Food Truck successfully removed!"});
+                    });
+                });
             });
         });
-    })
+
 
 
     // Add review for specific food truck
